8ee2f3222895c1149ede4c5e872ff97f16d2b5a7
Shane Burrell <shane@shaneburrell.com>
2026-07-24 00:25:46 -0400

    quiksync v0.4.0

--- Formula/quiksync.rb
+++ Formula/quiksync.rb
@@ -1,28 +1,28 @@
 class Quiksync < Formula
   desc "Resilient one-way file copy and sync with FastCDC deltas"
   homepage "https://github.com/shaneburrell/quiksync"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.3.0/quiksync_0.3.0_darwin_arm64.tar.gz"
-      sha256 "07386f10dade57affbff5a30cfff45fe9dbbcb79743882e14a2ccc93be397d12"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.4.0/quiksync_0.4.0_darwin_arm64.tar.gz"
+      sha256 "533138b809dae91288a7b012d334c37cd662f01c0c4ba084029604d2a5b314f2"
     end
     on_intel do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.3.0/quiksync_0.3.0_darwin_amd64.tar.gz"
-      sha256 "329273e31503818d6ed102fc3a59b7a4ce6837b7c3173e1b9e6f1d52e3fa508b"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.4.0/quiksync_0.4.0_darwin_amd64.tar.gz"
+      sha256 "ebff24d4d4ab34f0f37bcc1d71146b7d35bbd7a59d9ae7b7e0813de6bb8811a9"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.3.0/quiksync_0.3.0_linux_arm64.tar.gz"
-      sha256 "a5a237916e00272fbbc57ab63b5f014fb3e5c900572f0c42181126b0feda2e15"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.4.0/quiksync_0.4.0_linux_arm64.tar.gz"
+      sha256 "4e52e6d72c79dca189fed77402c96f02901fc222576df7b04a1eb8403370d755"
     end
     on_intel do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.3.0/quiksync_0.3.0_linux_amd64.tar.gz"
-      sha256 "093d852a7834c1943d70b0f6dd731e2f7a25f0722505ac2659fb98748e73fbc1"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.4.0/quiksync_0.4.0_linux_amd64.tar.gz"
+      sha256 "d0eb53175f663713a628939d13e18a2944377900ec207b743bbf11d8aa9e7bf3"
     end
   end
 
