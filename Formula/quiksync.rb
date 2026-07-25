7fcdb78f4484a0e66da4a5b08a4448868aa04719
Shane Burrell <shane@shaneburrell.com>
2026-07-24 23:26:19 -0400

    quiksync v0.5.0

--- Formula/quiksync.rb
+++ Formula/quiksync.rb
@@ -1,28 +1,28 @@
 class Quiksync < Formula
   desc "Resilient one-way file copy and sync with FastCDC deltas"
   homepage "https://github.com/shaneburrell/quiksync"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.4.0/quiksync_0.4.0_darwin_arm64.tar.gz"
-      sha256 "533138b809dae91288a7b012d334c37cd662f01c0c4ba084029604d2a5b314f2"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.0/quiksync_0.5.0_darwin_arm64.tar.gz"
+      sha256 "4481b824c915a7f3243bbfcc9f65f480707fa17349efa2898da8ff9df9e2d4a9"
     end
     on_intel do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.4.0/quiksync_0.4.0_darwin_amd64.tar.gz"
-      sha256 "ebff24d4d4ab34f0f37bcc1d71146b7d35bbd7a59d9ae7b7e0813de6bb8811a9"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.0/quiksync_0.5.0_darwin_amd64.tar.gz"
+      sha256 "d21038a9997f2e50e7cdcab8d2e4d55cf3d6a781a780918b395ece71d3b2dd28"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.4.0/quiksync_0.4.0_linux_arm64.tar.gz"
-      sha256 "4e52e6d72c79dca189fed77402c96f02901fc222576df7b04a1eb8403370d755"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.0/quiksync_0.5.0_linux_arm64.tar.gz"
+      sha256 "060d1a3fe039eb193495fb7c8c60a30862cfd1bc979a808c6cba134cafdb7046"
     end
     on_intel do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.4.0/quiksync_0.4.0_linux_amd64.tar.gz"
-      sha256 "d0eb53175f663713a628939d13e18a2944377900ec207b743bbf11d8aa9e7bf3"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.0/quiksync_0.5.0_linux_amd64.tar.gz"
+      sha256 "3166aff3195676077db47d717ea96e559b1feda48856169e1caa06a822d39cc9"
     end
   end
 
