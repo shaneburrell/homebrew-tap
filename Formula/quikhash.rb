5701286808ed1395cfef37bf06596a4303a6ad74
Shane Burrell <shane@shaneburrell.com>
2026-07-25 21:39:32 -0400

    quikhash v0.1.1

--- Formula/quikhash.rb
+++ Formula/quikhash.rb
@@ -1,28 +1,28 @@
 class Quikhash < Formula
   desc "FastCDC + BLAKE3 content-addressed hasher with reconstruction"
   homepage "https://github.com/shaneburrell/quikhash"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shaneburrell/quikhash/releases/download/v0.1.0/quikhash_0.1.0_darwin_arm64.tar.gz"
-      sha256 "ddd759f24b0b83578aea8e3cff00d5c4e3b08e3a3b766038bcd72ac4a2f0e5bd"
+      url "https://github.com/shaneburrell/quikhash/releases/download/v0.1.1/quikhash_0.1.1_darwin_arm64.tar.gz"
+      sha256 "3cb5f98cd21e9757551b421c6e5e9b526d4e2a9e5d49b4f72e94cabf53bfcbf1"
     end
     on_intel do
-      url "https://github.com/shaneburrell/quikhash/releases/download/v0.1.0/quikhash_0.1.0_darwin_amd64.tar.gz"
-      sha256 "dd2d4722be092e54afa0bec0b7bdcdeeface4db3ac94ebb877c03520c6b13b08"
+      url "https://github.com/shaneburrell/quikhash/releases/download/v0.1.1/quikhash_0.1.1_darwin_amd64.tar.gz"
+      sha256 "e67affd77b1ea7ca75cef67e6ae692ffcf6f3e94147f3aecb56ced374c0f087d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shaneburrell/quikhash/releases/download/v0.1.0/quikhash_0.1.0_linux_arm64.tar.gz"
-      sha256 "cdcc3da4f32ab79611cfcde28dd87f54a594ddf5104b6238ac2b90247dc02e75"
+      url "https://github.com/shaneburrell/quikhash/releases/download/v0.1.1/quikhash_0.1.1_linux_arm64.tar.gz"
+      sha256 "a2d7d1aad313fae8cccbca1dd1350f9a7296edf06b9dca7fb070e2b22a6ccd0f"
     end
     on_intel do
-      url "https://github.com/shaneburrell/quikhash/releases/download/v0.1.0/quikhash_0.1.0_linux_amd64.tar.gz"
-      sha256 "196451518e363d8f1dcbfe61958c7d1e8e554514eb0984161acde0e74aba60e0"
+      url "https://github.com/shaneburrell/quikhash/releases/download/v0.1.1/quikhash_0.1.1_linux_amd64.tar.gz"
+      sha256 "33464b99370bd2e2cfc9e58d100265209e462c824868868f2af39cafda50c41d"
     end
   end
 
