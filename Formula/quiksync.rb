12d1f3f29aaa361df0f63808b0225164c4900e9e
Shane Burrell <shane@shaneburrell.com>
2026-07-31 22:05:57 -0400

    quiksync v0.5.4

--- Formula/quiksync.rb
+++ Formula/quiksync.rb
@@ -1,28 +1,28 @@
 class Quiksync < Formula
   desc "Resilient one-way file copy and sync with FastCDC deltas"
   homepage "https://github.com/shaneburrell/quiksync"
-  version "0.5.3"
+  version "0.5.4"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.3/quiksync_0.5.3_darwin_arm64.tar.gz"
-      sha256 "4872db1f93d082498ebd78caed601110370edf106ab4f9b21c36d98a04bf61e6"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.4/quiksync_0.5.4_darwin_arm64.tar.gz"
+      sha256 "b2528ea9f90d7183ed70df9a54bfd42513029a0bab9cd3e2793e67217525bf61"
     end
     on_intel do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.3/quiksync_0.5.3_darwin_amd64.tar.gz"
-      sha256 "db7f47f934df06c2c32dc7b7bf6057469326433382f88fe76f0a87dfa27ea5f8"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.4/quiksync_0.5.4_darwin_amd64.tar.gz"
+      sha256 "59be06a1574ee343445f88922b67d486173d97c97e8451c37888f2d2de3b3c6f"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.3/quiksync_0.5.3_linux_arm64.tar.gz"
-      sha256 "77078e5ad909bc39259a627adb6d7f4d34d8b49938e53f9d124ed17d409fc9b7"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.4/quiksync_0.5.4_linux_arm64.tar.gz"
+      sha256 "ec3f740d4c04f115cf22e7e8ac03b473d10f40a36d7a7722c1a256afec88f88a"
     end
     on_intel do
-      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.3/quiksync_0.5.3_linux_amd64.tar.gz"
-      sha256 "8b575bb2b03e5ea759e1a693ecc0044fab73718e469d6defa9fbedaa5948a64b"
+      url "https://github.com/shaneburrell/quiksync/releases/download/v0.5.4/quiksync_0.5.4_linux_amd64.tar.gz"
+      sha256 "ce71c11880bd70b3b617bffd4b38ef27f61b3f40f4d9d576e1001ad28e561b8d"
     end
   end
 
