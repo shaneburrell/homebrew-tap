cf2a9ed928e94515aacfa9f2cf89a5c36027db13
Shane Burrell <shane@shaneburrell.com>
2026-07-31 23:00:59 -0400

    modelmove v0.1.3

--- Formula/modelmove.rb
+++ Formula/modelmove.rb
@@ -1,28 +1,28 @@
 class Modelmove < Formula
   desc "Sparse-delta, verified transfer and migration of LLM model weights"
   homepage "https://github.com/shaneburrell/modelmove"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shaneburrell/modelmove/releases/download/v0.1.2/modelmove_0.1.2_darwin_arm64.tar.gz"
-      sha256 "41b5602f40d6a571d53c166226e35abfae9fb6a0b86a8e2fbfc0ff31c03ca405"
+      url "https://github.com/shaneburrell/modelmove/releases/download/v0.1.3/modelmove_0.1.3_darwin_arm64.tar.gz"
+      sha256 "fb03fe1a8cb74092f14f8b2c617fd3c4e122960751cb5a5b1ad723e875922556"
     end
     on_intel do
-      url "https://github.com/shaneburrell/modelmove/releases/download/v0.1.2/modelmove_0.1.2_darwin_amd64.tar.gz"
-      sha256 "6df09240d3bda85e72bb888e920ddc7047534edf646d6cbb7d15ceeafd4a33c0"
+      url "https://github.com/shaneburrell/modelmove/releases/download/v0.1.3/modelmove_0.1.3_darwin_amd64.tar.gz"
+      sha256 "40b9f290b0e0d05ae64a45bb64d3a6f1020256ff00acb61b597570cfdf3cc8e9"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shaneburrell/modelmove/releases/download/v0.1.2/modelmove_0.1.2_linux_arm64.tar.gz"
-      sha256 "49110cc9faf67afbd8e62e5c05f859441a1a5ad4ecf847faf202b13e9ef4d43e"
+      url "https://github.com/shaneburrell/modelmove/releases/download/v0.1.3/modelmove_0.1.3_linux_arm64.tar.gz"
+      sha256 "439dcf8c43f786a2d3515d922236748905a62c24b7887260f576dd71688baed9"
     end
     on_intel do
-      url "https://github.com/shaneburrell/modelmove/releases/download/v0.1.2/modelmove_0.1.2_linux_amd64.tar.gz"
-      sha256 "11a49286b56146d5d7c0d8f2198cc8f8baac7c2c4d4297a42bb0b6e3bf6879af"
+      url "https://github.com/shaneburrell/modelmove/releases/download/v0.1.3/modelmove_0.1.3_linux_amd64.tar.gz"
+      sha256 "df2ac05ef0f8634bcb59c90f936314e29dffeee597b4076db587c7fba05b5210"
     end
   end
 
