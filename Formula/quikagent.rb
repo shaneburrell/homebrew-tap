20e6a7a2231e86d16772d460085808f1d467d8e8
Shane Burrell <shane@shaneburrell.com>
2026-08-29 04:15:23 -0400

    Bump quikagent to 0.1.7.

--- Formula/quikagent.rb
+++ Formula/quikagent.rb
@@ -1,28 +1,28 @@
 class Quikagent < Formula
   desc "Minimal terminal coding agent"
   homepage "https://github.com/shaneburrell/quikagent"
-  version "0.1.6"
+  version "0.1.7"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shaneburrell/quikagent/releases/download/v0.1.6/quikagent_0.1.6_darwin_arm64.tar.gz"
-      sha256 "6de9fd09398b574111fef0ab613a586e4726fd49161c1c0970e0de710f1cb39f"
+      url "https://github.com/shaneburrell/quikagent/releases/download/v0.1.7/quikagent_0.1.7_darwin_arm64.tar.gz"
+      sha256 "7600f8dab741e8e7e7f4c4f2cb82c68904ab7fdf94484d8f6eca4ba96fbd65b6"
     end
     on_intel do
-      url "https://github.com/shaneburrell/quikagent/releases/download/v0.1.6/quikagent_0.1.6_darwin_amd64.tar.gz"
-      sha256 "6a587d9a64f279bf55f3c32b5d2e2405b4d1992019ff53ddbc9444a34c798f29"
+      url "https://github.com/shaneburrell/quikagent/releases/download/v0.1.7/quikagent_0.1.7_darwin_amd64.tar.gz"
+      sha256 "c12147d03d6c6d0ba3102f93b0dcd7085b614d06984217d020256e0f3cccc80c"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shaneburrell/quikagent/releases/download/v0.1.6/quikagent_0.1.6_linux_arm64.tar.gz"
-      sha256 "15c902e861222c1f2e300d37210e9c9cb9331f43d3cf11ef955f50416992c22c"
+      url "https://github.com/shaneburrell/quikagent/releases/download/v0.1.7/quikagent_0.1.7_linux_arm64.tar.gz"
+      sha256 "faeeb477a007511f49f484c4652b22150a2d09aa93f657192d657d4fe2e1d3d9"
     end
     on_intel do
-      url "https://github.com/shaneburrell/quikagent/releases/download/v0.1.6/quikagent_0.1.6_linux_amd64.tar.gz"
-      sha256 "be04eda73675463c6ff946a5d7285589a931e0474b88f00ffa4b313ca69d5d15"
+      url "https://github.com/shaneburrell/quikagent/releases/download/v0.1.7/quikagent_0.1.7_linux_amd64.tar.gz"
+      sha256 "bf4adcdb42f2ff84a94b05af55b65ddc4c20024d63161eab3e9d1f030fb980ee"
     end
   end
 
